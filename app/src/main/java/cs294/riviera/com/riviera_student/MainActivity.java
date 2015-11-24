069f66bcc014e1f2a1314b71db044c9703c0bb47
Tricia Fu <triciasfu@gmail.com>
2015-11-23 21:59:55 -0800

    sign up page

--- app/src/main/java/cs294/riviera/com/riviera_student/MainActivity.java
+++ app/src/main/java/cs294/riviera/com/riviera_student/MainActivity.java
@@ -1,36 +1,122 @@
 package cs294.riviera.com.riviera_student;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.nio.charset.Charset;
 
 public class MainActivity extends AppCompatActivity {
 
     private Tag mTag;
+    private Button resumeButton;
+    private Button submitButton;
+
+    private EditText studentName;
+    private EditText studentEmail;
+    private EditText studentGrad;
+    private EditText studentWebsite;
+    private EditText studentLinkedin;
+
+    private String nameText;
+    private String emailText;
+    private String gradText;
+    private String websiteText;
+    private String linkedinText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resumeButton = (Button) findViewById(R.id.resume_button);
+        submitButton = (Button) findViewById(R.id.submit_button);
+
+        studentName = (EditText) findViewById(R.id.student_name);
+        studentEmail = (EditText) findViewById(R.id.student_email);
+        studentGrad = (EditText) findViewById(R.id.student_grad);
+        studentWebsite = (EditText) findViewById(R.id.student_website);
+        studentLinkedin = (EditText) findViewById(R.id.student_linkedin);
+    }
+
+    public void handleResumeButton(View view) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        startActivity(intent);
+    }
+
+    public void handleSubmitButton(View view) {
+        nameText = studentName.getText().toString();
+        emailText = studentEmail.getText().toString();
+        gradText = studentGrad.getText().toString();
+        websiteText = studentWebsite.getText().toString();
+        linkedinText = studentLinkedin.getText().toString();
+
+        Boolean cancel = signUpCheck();
+
+        if (!cancel) {
+            displayOk("Success!");
+        } else {
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean signUpCheck() {
+        View focusView = null;
+        Boolean cancel = false;
+        if (TextUtils.isEmpty(nameText)) {
+            studentName.setError(getString(R.string.error_field_required));
+            focusView = studentName;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(emailText)) {
+            studentEmail.setError(getString(R.string.error_field_required));
+            focusView = studentEmail;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(gradText)) {
+            studentGrad.setError(getString(R.string.error_field_required));
+            focusView = studentGrad;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(websiteText)) {
+            studentWebsite.setError(getString(R.string.error_field_required));
+            focusView = studentWebsite;
+            cancel = true;
+        }
+        if (TextUtils.isEmpty(linkedinText)) {
+            studentLinkedin.setError(getString(R.string.error_field_required));
+            focusView = studentLinkedin;
+            cancel = true;
+        }
+        return cancel;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-
         mTag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
-
         NdefRecord uriRecord = new NdefRecord(
                 NdefRecord.TNF_ABSOLUTE_URI ,
                 "http://developer.android.com/index.html".getBytes(Charset.forName("US-ASCII")),
                 new byte[0], new byte[0]);
-
     }
 
     @Override
@@ -51,7 +137,31 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Displays a toast with a MESSAGE. */
+    public void displayOk(String message) {
+        hideSoftKeyboard(this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Thanks for signing up! Please pick up your badge.")
+                .setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        studentName.setText("");
+                        studentEmail.setText("");
+                        studentGrad.setText("");
+                        studentWebsite.setText("");
+                        studentLinkedin.setText("");
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /** Dismisses the soft-key board outside of EditText area. */
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
 }
